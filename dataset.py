24c586179f0467bc33d18bb938fd9ebc518e012c
Michal Kodad <misa.kodad@gmail.com>
2025-05-06 03:32:30 +0200

    Upgraded dataset playground

--- dataset.py
+++ dataset.py
@@ -75,63 +75,73 @@ def ocr_with_paddle(img):
     finaltext = ''
     font_path = 'simfang.ttf' # PaddleOCR
 
-    table_engine = PPStructure(show_log=True,
-                               layout_score_threshold=0.3,
-                               layout_nms_threshold=0.5,
-                               table=False,
-                               image_orientation=False,
-                               ocr=True,
-                               lang='en',
-                               merge_no_span_structure=False,
-        )
+    if False:
+        table_engine = PPStructure(show_log=True,
+                                layout_score_threshold=0.3,
+                                layout_nms_threshold=0.5,
+                                table=False,
+                                image_orientation=False,
+                                ocr=True,
+                                lang='en',
+                                merge_no_span_structure=False,
+            )
 
-    save_folder = './output'
-    result = table_engine(img)
-    save_structure_res(result, save_folder, "output")
+        save_folder = './output'
+        result = table_engine(img)
+        save_structure_res(result, save_folder, "output")
 
-    for line in result:
-        line.pop('img')
-        print(line)
+        for line in result:
+            line.pop('img')
+            print(line)
 
-    from PIL import Image
+        from PIL import Image
 
-    im_show = draw_structure_result(img, result,font_path=font_path)
-    convertor.show_image(im_show)
-    im_show = Image.fromarray(im_show)
-    im_show.save(save_folder + '/result.jpg')
-    return
+        im_show = draw_structure_result(img, result,font_path=font_path)
+        convertor.show_image(im_show)
+        im_show = Image.fromarray(im_show)
+        im_show.save(save_folder + '/result.jpg')
+    else:
+        ocr = PaddleOCR(
+            lang='en',
+            use_angle_cls=True)
+        result = ocr.ocr(img, det=True, rec=True)
 
-    # ocr = PaddleOCR(
-    #     lang='en',
-    #     use_angle_cls=True)
-    # result = ocr.ocr(img, det=True, rec=False)
+        # for i in range(len(result[0])):
+        #     text = result[0][i][1][0]
+        #     finaltext += ' '+ text
 
-    # for i in range(len(result[0])):
-    #     text = result[0][i][1][0]
-    #     finaltext += ' '+ text
+        from PIL import Image
+        result = result[0]
+        image = Image.fromarray(img).convert('RGB')
+        boxes = [line[0] for line in result]
+        txts = [line[1][0] for line in result]
+        scores = [line[1][1] for line in result]
+        im_show = draw_ocr(image, boxes, txts, scores, font_path=font_path)
+        print(boxes)
+        print(txts)
+        # result_structure = [
+        #     {"type": "title", "bbox": [*line[0], *line[2]], "res": "", "img_idx": 0, "score": 0.8120958805084229}
+        #     for line in result]
+        # im_show = draw_structure_result(img, result_structure,font_path=font_path)
+        convertor.show_image(im_show)
+        im_show = Image.fromarray(im_show)
+        im_show.save('result.jpg')
 
-    # from PIL import Image
-    # result = result[0]
-    # image = Image.fromarray(img).convert('RGB')
-    # boxes = [line[0] for line in result]
-    # # txts = [line[1][0] for line in result]
-    # # scores = [line[1][1] for line in result]
-    # # im_show = draw_ocr(image, boxes, txts, scores, font_path=font_path)
-    # result_structure = [
-    #     {"type": "title", "bbox": [*line[0], *line[2]], "res": "", "img_idx": 0, "score": 0.8120958805084229}
-    #     for line in result]
-    # im_show = draw_structure_result(img, result_structure,font_path=font_path)
-    # convertor.show_image(im_show)
-    # im_show = Image.fromarray(im_show)
-    # im_show.save('result.jpg')
-
-    # return finaltext
+        # return finaltext
 
 
 def main(args):
     r = RecipeDataset(generate_images=True)
 
-    only_images = True if not args.use_paddle else False
+    # Run for concrete img
+    # only_images = True if not args.use_paddle else False
+    # from PIL import Image
+    # img = torchvision.transforms.functional.to_tensor(Image.open("recipe_edited/griddle-recipe-book-2_edited.jpg"))
+    # img *= 255
+    # img = img.type(torch.uint8)
+    # img = img.permute(1, 2, 0).numpy()
+    # ocr_with_paddle(img)
+    # return
 
     for data in r:
         img, title, ingredients, instructions = data
